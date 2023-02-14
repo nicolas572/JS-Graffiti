5707b1c135233003164ea2c5827ec09e50b417cd
Nicolas Nueca <nicolasnueca@nicolass-air.lan>
2023-02-13 18:47:26 -0800

    changed header text-align to left

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Select some elements...
+//Step 1 find your selector from the index.html file and declare it a variable
+let pageHeader = document.querySelector('#page-header') ;
+
+//Step 2 what do you want to style? pick a CSS property that you want to change
+//text-align => textAlign
+
+//Step 3 write the styling code using DOM
+pageHeader.style.textAlign = 'left'
